07267c0785fcabe50911ea2c113f08e17a52ca80
zkrakus <zkrakus@gmail.com>
2025-09-08 16:58:42 -0400

    feat:Simple useState form with event on change and form submit.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,58 @@
 export default function Login() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault(); // Not needed for button type="button"
+  }
+
+  function handleEmailChange(event) {
+    setEnteredEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setEnteredPassword(event.target.value);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleEmailChange}
+            value={enteredEmail}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handlePasswordChange}
+            value={enteredPassword}
+          />
         </div>
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button type="button" className="button button-flat">
+          Reset
+        </button>
+        <button
+          // type="button"
+          className="button"
+          onClick={handleSubmit}
+        >
+          Login
+        </button>
       </p>
     </form>
   );
